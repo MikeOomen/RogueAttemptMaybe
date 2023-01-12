b9cad5a5169e20ee3d22fdd0c3222d9cdab49352
Mike-SDG22A <113519352+Mike-SDG22A@users.noreply.github.com>
2023-01-12 12:11:44 +0100

    Add project files.

---
+++ RogueAttemptMaybe/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace RogueAttemptMaybe
+{
+    //To-Do list:
+    //Map
+    //GUI
+    //Random (bruikbare) levels van 64x32 characters.
+
+    //Player:
+    //Move 1 space with arrows
+    //HP
+    //Money
+    //Attack
+    //Inventory
+
+    //Enemies:
+    //Movement
+    //HP
+    //Attack
+
+    //MaybeMaybeMaybe:
+    //Different types of enemies?
+    //Classes?
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
